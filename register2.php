fe249a2ba6a9c244b7ce9683e7bcb3a7902814af
Sine997 <103395877+Sine997@users.noreply.github.com>
2022-11-29 13:30:38 +0800

    Add files via upload

---
+++ register2.php
@@ -0,0 +1,193 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $address = $salary = $username = $password ="";
+$name_err = $address_err = $salary_err = $username_err =$password_err ="";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate address
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)){
+        $address_err = "Please enter an address.";     
+    } else{
+        $address = $input_address;
+    }
+    
+    // Validate salary
+    $input_salary = trim($_POST["salary"]);
+    if(empty($input_salary)){
+        $salary_err = "Please enter the salary amount.";     
+    } elseif(!ctype_digit($input_salary)){
+        $salary_err = "Please enter a positive integer value.";
+    } else{
+        $salary = $input_salary;
+    }
+
+    // Validate username
+if(empty(trim($_POST["username"]))){
+    $username_err = "Please enter a username.";
+} elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+    $username_err = "Username can only contain letters, numbers, and underscores.";
+} else{
+    // Prepare a select statement
+    $sql = "SELECT id FROM employees WHERE username = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_username);
+        
+        // Set parameters
+        $param_username = trim($_POST["username"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            /* store result */
+            mysqli_stmt_store_result($stmt);
+            
+            if(mysqli_stmt_num_rows($stmt) == 1){
+                $username_err = "This username is already taken.";
+            } else{
+                $username = trim($_POST["username"]);
+            }
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+}
+
+// Validate confirm password
+if(empty(trim($_POST["password"]))){
+    $password_err = "Please confirm password.";     
+} else{
+    $password = trim($_POST["password"]);
+    if(empty($password_err) && ($password != $password)){
+        $password_err = "Password did not match.";
+    }
+}
+ 
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($address_err) && empty($salary_err)&& empty($username_err)&& empty($password_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO employees (name, address, salary, username, password) VALUES (?, ?, ?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ssiss", $param_name, $param_address, $param_salary, $param_username, $param_password);
+            
+            // Set parameters
+            $param_name = $name;
+            $param_address = $address;
+            $param_salary = $salary;
+            $param_username = $username;
+            $param_password = $password;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: register2.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+
+
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+    integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
+    crossorigin="anonymous" />
+	
+<link href="style.css" rel="stylesheet" type="text/css">
+
+
+</head>
+<body>
+<button class="nav-btn open-btn">
+    <i class="fas fa-bars"></i>
+  </button>
+  <div id="header-wrapper"></div>
+
+
+
+  <div class="nav nav-black">
+    <div class="nav nav-red">
+      <div class="nav nav-white">
+        <button class="nav-btn close-btn">
+          <i class="fas fa-times"></i>
+        </button>
+        
+        <img
+          src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fphoto.16pic.com%2F00%2F01%2F29%2F16pic_129006_b.jpg&refer=http%3A%2F%2Fphoto.16pic.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1671845959&t=50a5a9635603f6e1feb3169133f50c74"
+          alt="Logo" class="logo">
+     
+        <ul class="list">
+          <ul>
+          <li><a href="index.html" accesskey="1" title="">Home</a></li>
+            <li><a href="aboutus.html" accesskey="2" title="">About US</a></li>
+            <li><a href="upload.php" accesskey="3" title="">Careers </a></li>
+            <li><a href="orderonline.php" accesskey="4" title="">Order online </a></li>
+            <li><a href="contactus.html" accesskey="5" title="">Contact Us</a></li>
+            <li class="active"><a href="#" accesskey="6" title="">Register</a></li>
+          </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                <h2 class="mt-5">Registration successful</h2>
+                    <a href="register.php" >Back to register</a>
+                    
+                </div>
+            </div>        
+        </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br>
+    <footer class="footer2">  
+    Lucca bread, whatever you want
+          </footer>
+          <script src="script.js"></script>
+</body>
+
+	
+
+
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,97 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+    integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
+    crossorigin="anonymous" />
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+
+</head>
+<body>
+<button class="nav-btn open-btn">
+    <i class="fas fa-bars"></i>
+  </button>
+  <div id="header-wrapper"></div>
+  <div class="nav nav-black">
+    <div class="nav nav-red">
+      <div class="nav nav-white">
+        <button class="nav-btn close-btn">
+          <i class="fas fa-times"></i>
+        </button>
+        
+        <img
+          src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fphoto.16pic.com%2F00%2F01%2F29%2F16pic_129006_b.jpg&refer=http%3A%2F%2Fphoto.16pic.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1671845959&t=50a5a9635603f6e1feb3169133f50c74"
+          alt="Logo" class="logo">
+     
+        <ul class="list">
+          <ul>
+          <li><a href="index.html" accesskey="1" title="">Home</a></li>
+            <li><a href="aboutus.html" accesskey="2" title="">About US</a></li>
+            <li class="active"><a href="#" accesskey="3" title="">Careers</a></li>
+            <li><a href="orderonline.php" accesskey="4" title="">Order online </a></li>
+            <li><a href="contactus.html" accesskey="5" title="">Contact Us</a></li>
+            <li><a href="register.php" accesskey="6" title="">register</a></li>
+          </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+<div class="wrapper">
+	<div id="welcome" class="container">
+		<div class="title">
+            <h1>Join our team</h1>
+            <p> We are expanding our branches and need experienced baker regularly.</n>
+                Send us your detail with the form below</br>
+            and we will be in contact shortly</p>
+
+            <div id="three-column" class="container">
+           
+                <div class="title">
+                    <h2>Fill out the form below and upload your cover letter and resume</h2>
+                    <form action="file_upload.php" method="POST"
+            enctype="multipart/form-data">
+ 
+
+         
+        <label2 for="name">First name:</label2>
+        <input type="text" id="name" name="name"/><br>
+        <label2 for="email">Last name:</label2>
+        <input type="text" id="email"name="email" /><br>
+        <label2 for="telephone">Email Address:</label2>
+        <input type="text" id="telephone" name="telephone"/><br>
+        <label2 for="message">Work experience</label2>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea><br />
+<p>
+            Select files to upload:
+             
+            <!-- name of the input fields are going to
+                be used in our php script-->
+            <input type="file" name="files[]" multiple><br>
+            <input type="file" name="files[]" multiple>
+            <br><br>
+             
+            <input type="submit" name="submit" v+++++++++++++++alue="Upload" >
+        </p>
+ 
+    </form>
+                
+                </div>
+            
+				
+				
+            </div>
+            </div>
+            </div>
+            </div>
+<br><br><br><br><br><br><br><br><br><br>
+            <footer  class="footer2">  
+            ChenJiu Joey 193190917
+          </footer>
+          <script src="script.js"></script>
+</body>
+</html>
+
+           
+  
